dbe278f641b420bc7a8164ddab60ea21de538a15
Oliver Hartkopp <socketcan@hartkopp.net>
2022-12-17 14:50:23 +0100

    printxlframe: print 12 instead of only 8 data bytes
    Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>

--- canxlrcv.c
+++ canxlrcv.c
@@ -29,8 +29,8 @@ static void printxlframe(struct canxl_frame *cfx)
 	printf("%03X###%02X%02X%08X",
 	       cfx->prio, cfx->flags, cfx->sdt, cfx->af);
 
-	/* print up to 8 data bytes */
-	for (i = 0; i < cfx->len && i < 8; i++)
+	/* print up to 12 data bytes */
+	for (i = 0; i < cfx->len && i < 12; i++)
 		printf("%02X", cfx->data[i]);
 
 	/* print CAN XL data length */
