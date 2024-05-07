66fc3f2ac7aad18100bd0df1c933dd429b6eb3f6
Oliver Hartkopp <socketcan@hartkopp.net>
2024-05-07 21:10:37 +0200

    add testcase_11 script and log file
    Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>

---
+++ test/build_testcase_11.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# run 'candump -l vcanxl0' to create a logfile with the plugfest replay
+
+CANXLGEN=../canxlgen
+CIA613FRAG=../cia613frag
+
+TESTIF=vcanxl0
+UNFRAGIF=vcanxl1
+FS=128
+
+# start fragmenting process with required fragment size
+$CIA613FRAG -t 001 -f $FS $UNFRAGIF $TESTIF &
+FRAGPID=$!
+
+# create test data frame and segmented traffic
+$CANXLGEN $TESTIF -P -S 0 -A 0 -p 401 -l 1024:1024
+$CANXLGEN $UNFRAGIF -P -S 0 -A 0 -p 001 -l 1024:1024
+
+sleep 1
+
+# stop fragmenting process with required fragment size
+kill $FRAGPID
+
+# start fragmenting process with required fragment size
+$CIA613FRAG -t 021 -f $FS $UNFRAGIF $TESTIF &
+FRAGPID=$!
+
+# create test data frame and segmented traffic
+$CANXLGEN $TESTIF -P -S 0 -A 0 -p 421 -l 512:512
+$CANXLGEN $UNFRAGIF -P -S 0 -A 0 -p 021 -l 512:512
+
+sleep 1
+
+# stop fragmenting process with required fragment size
+kill $FRAGPID
